a26f40bb0e878639223ba9dd3ce2e8af52c4b0d9
Lenovo <Lenovo@DESKTOP-02AKGBB>
2022-06-25 22:58:44 +0300

    Add project files.

---
+++ secondLAB assignment/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+Console.WriteLine("welcome to our grade system");
+Console.WriteLine("Enter the number of students");
+int n = int.Parse(Console.ReadLine());
+
+
+int[] Grade = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Enter your Grade for Student " + (i + 1));
+
+    Grade[i] = int.Parse(Console.ReadLine());
+    if (Grade[i] > 100 || Grade[i] < 0)
+    {
+        Console.WriteLine("No such grade!please Renter!:");
+        Grade[i] = int.Parse(Console.ReadLine());
+    }
+
+    {
+        if (Grade[i] > 89)
+        {
+            Console.WriteLine(" Your Grade is 'A'");
+        }
+        else if (Grade[i] > 79)
+        {
+            Console.WriteLine(" Your Grade is 'B'");
+        }
+        else if (Grade[i] > 69)
+        {
+            Console.WriteLine(" Your Grade is 'C'");
+        }
+        else if (Grade[i] > 59)
+        {
+            Console.WriteLine(" Your Grade is 'D'");
+        }
+        else
+        {
+            Console.WriteLine(" Your Grade is 'F'");
+        }
+    }
+}
